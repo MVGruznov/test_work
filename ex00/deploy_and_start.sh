830c054fd57e9b22d237775c592b11c441fa6c13
Mikhail Gruznov <soulfocus@yandex.ru>
2020-10-02 23:06:06 +0300

    fixed deploy

--- ex00/deploy_and_start.sh
+++ ex00/deploy_and_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo -e '#!/bin/bash\nwhile :\ndo\n\techo "hello world!!!"\n\tsleep 10\ndone' > /bin/hw_script.sh
 chmod +x /bin/hw_script.sh
-echo -e '[Unit]\n\tDescription= start hello world script\n[Service]\n\tExecStart=/usr/bin/hw_script.sh\n\tType=simple\n\tStandartOutput=tty' > /lib/systemd/system/hw.service
+echo -e '[Unit]\n\tDescription= start hello world script\n[Service]\n\tExecStart=/usr/bin/hw_script.sh\n\tType=simple\n\tStandardOutput=tty' > /lib/systemd/system/hw.service
 systemctl start hw
